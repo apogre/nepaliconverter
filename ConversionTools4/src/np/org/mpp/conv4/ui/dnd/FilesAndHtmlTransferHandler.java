7af1df0061d54071db14814a027f211e65cd6e22
jacob.nordfalk <jacob.nordfalk@f0521fda-cb4d-0410-be50-dbe01dfbe39f>
2008-06-26 08:08:08 +0000

    <No Comment Entered>

--- ConversionTools4/src/np/org/mpp/conv4/ui/dnd/FilesAndHtmlTransferHandler.java
+++ ConversionTools4/src/np/org/mpp/conv4/ui/dnd/FilesAndHtmlTransferHandler.java
@@ -1,12 +1,11 @@
 package np.org.mpp.conv4.ui.dnd;
 
 
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
+import java.awt.Dimension;
+import java.awt.datatransfer.*;
 import java.io.*;
 
 import javax.swing.*;
-import java.awt.*;
 
 
 
@@ -32,14 +31,17 @@ import java.awt.*;
 public class FilesAndHtmlTransferHandler extends TransferHandler {
   public JEditorPane console;
 
-  public DataFlavor fileDf, htmlDf;
+  public DataFlavor fileDf, htmlDfByteUtf8;
 
   public FilesAndHtmlTransferHandler() {
+      StringSelection s;
       try {
           //fileDf = new DataFlavor("text/uri-list; class=java.lang.String");
           //htmlDf = new DataFlavor("text/html; class=java.lang.String"); // charset=unicode;
-          fileDf = new DataFlavor("text/uri-list; charset=UTF-8; class=java.lang.String");
-          htmlDf = new DataFlavor("text/html; charset=UTF-8; class=java.lang.String"); // charset=unicode;
+          fileDf = new DataFlavor("text/uri-list; class=java.lang.String");
+
+          // Works fine Ubuntu Linux 8.04, jdk1.6.0, from OpenOffice.org 2.4.1:
+          htmlDfByteUtf8 = new DataFlavor("text/html; class=\"[B\"; charset=UTF-8"); // charset=unicode; charset=UTF-8
       } catch (ClassNotFoundException ex) {
           ex.printStackTrace();
       }
@@ -49,10 +51,10 @@ public class FilesAndHtmlTransferHandler extends TransferHandler {
     FilesAndHtmlTransferHandler th = new FilesAndHtmlTransferHandler();
 
     // Observes the selection (copy/cut in text documents detected)
-    new ClipboardObserver(Toolkit.getDefaultToolkit().getSystemClipboard(), th.htmlDf);
+    //new ClipboardObserver(Toolkit.getDefaultToolkit().getSystemClipboard(), th.htmlDfByteUtf8);
 
     // Observes the selection (also without copy)
-    new ClipboardObserver(Toolkit.getDefaultToolkit().getSystemSelection(), DataFlavor.stringFlavor);
+    //new ClipboardObserver(Toolkit.getDefaultToolkit().getSystemSelection(), DataFlavor.stringFlavor);
     th.console = new JEditorPane("text/plain", "drag files/html here");
     th.console.setPreferredSize(new Dimension(600,200));
     th.console.setTransferHandler(th);
@@ -70,7 +72,10 @@ public class FilesAndHtmlTransferHandler extends TransferHandler {
 
 
   public boolean importData(JComponent comp, Transferable t) {
+      StringBuffer status = new StringBuffer();
+      logAllDataFlavors(t, status);
 
+      System.out.println(status);
 
     try {
 
@@ -82,10 +87,9 @@ public class FilesAndHtmlTransferHandler extends TransferHandler {
         }
 
 
-        if (t.isDataFlavorSupported(htmlDf)) {
+        if (t.isDataFlavorSupported(htmlDfByteUtf8)) {
             //status.append("HRML"+htmlDf);
-            String s = (String) t.getTransferData(htmlDf);
-            System.out.println("s = " + s);
+            String s = getHtml(t);
             console.setText("HTML\n"+s);
             return true;
         }
@@ -97,9 +101,6 @@ public class FilesAndHtmlTransferHandler extends TransferHandler {
 
 
 
-    StringBuffer status = new StringBuffer();
-
-    logAllDataFlavors(t, status);
     /*
     if (t.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
         status.append("FILE LIST!");
@@ -122,7 +123,27 @@ public class FilesAndHtmlTransferHandler extends TransferHandler {
     return true;
   }
 
-  private void logAllDataFlavors(Transferable t, StringBuffer status) {
+  public String getHtml(Object transferData) throws IOException, UnsupportedFlavorException {
+      byte[] b = (byte[]) transferData;
+      String s = new String(b, "UTF-8");
+      System.out.println("s = " + s);
+
+      StringBuffer sb = new StringBuffer(s.length());
+      for (int i=0; i<s.length(); i++) {
+        char ch = s.charAt(i);
+        //System.out.println((int) ch +" "+ ch);
+        if (ch != 0) sb.append(ch);
+      }
+      return s.toString();
+  }
+
+
+  public String getHtml(Transferable t) throws IOException, UnsupportedFlavorException {
+      return getHtml(t.getTransferData(htmlDfByteUtf8));
+  }
+
+
+  public static void logAllDataFlavors(Transferable t, StringBuffer log) {
       //support.getDataFlavors()
 
       DataFlavor[] fl = t.getTransferDataFlavors();
@@ -133,7 +154,7 @@ public class FilesAndHtmlTransferHandler extends TransferHandler {
       for (int i = 0; i < fl.length; i++) {
         DataFlavor f = fl[i];
 
-        status.append("f[" + i + "]=" + f.getMimeType() + "\n");
+        log.append("f[" + i + "]=" + f.getMimeType() + "\n");
 
         try {
           System.err.println();
@@ -190,4 +211,167 @@ public class FilesAndHtmlTransferHandler extends TransferHandler {
       }
   }
 
+
 }
+/*  TRANSFER HANDLER LOGS
+
+Ubuntu Linux 8.04, jdk1.6.0, from OpenOffice.org 2.4.1:
+f[0]=application/x-openoffice-link; windows_formatname=Link; class=java.io.InputStream
+f[1]=application/x-openoffice-objectdescriptor-xml; windows_formatname="Star Object Descriptor (XML)"; class=java.io.InputStream
+f[2]=application/x-openoffice-embed-source-xml; windows_formatname="Star Embed Source (XML)"; class=java.io.InputStream
+f[3]=text/html; class=java.io.Reader; charset=Unicode
+f[4]=text/html; class=java.lang.String; charset=Unicode
+f[5]=text/html; class=java.nio.CharBuffer; charset=Unicode
+f[6]=text/html; class="[C"; charset=Unicode
+f[7]=text/html; class=java.io.InputStream; charset=UTF-16
+f[8]=text/html; class=java.nio.ByteBuffer; charset=UTF-16
+f[9]=text/html; class="[B"; charset=UTF-16
+f[10]=text/html; class=java.io.InputStream; charset=UTF-8
+f[11]=text/html; class=java.nio.ByteBuffer; charset=UTF-8
+f[12]=text/html; class="[B"; charset=UTF-8
+f[13]=text/html; class=java.io.InputStream; charset=UTF-16BE
+f[14]=text/html; class=java.nio.ByteBuffer; charset=UTF-16BE
+f[15]=text/html; class="[B"; charset=UTF-16BE
+f[16]=text/html; class=java.io.InputStream; charset=UTF-16LE
+f[17]=text/html; class=java.nio.ByteBuffer; charset=UTF-16LE
+f[18]=text/html; class="[B"; charset=UTF-16LE
+f[19]=text/html; class=java.io.InputStream; charset=ISO-8859-1
+f[20]=text/html; class=java.nio.ByteBuffer; charset=ISO-8859-1
+f[21]=text/html; class="[B"; charset=ISO-8859-1
+f[22]=text/html; class=java.io.InputStream; charset=US-ASCII
+f[23]=text/html; class=java.nio.ByteBuffer; charset=US-ASCII
+f[24]=text/html; class="[B"; charset=US-ASCII
+f[25]=text/richtext; class=java.io.Reader; charset=Unicode
+f[26]=text/richtext; class=java.lang.String; charset=Unicode
+f[27]=text/richtext; class=java.nio.CharBuffer; charset=Unicode
+f[28]=text/richtext; class="[C"; charset=Unicode
+f[29]=text/richtext; class=java.io.InputStream; charset=UTF-16
+f[30]=text/richtext; class=java.nio.ByteBuffer; charset=UTF-16
+f[31]=text/richtext; class="[B"; charset=UTF-16
+f[32]=text/richtext; class=java.io.InputStream; charset=UTF-8
+f[33]=text/richtext; class=java.nio.ByteBuffer; charset=UTF-8
+f[34]=text/richtext; class="[B"; charset=UTF-8
+f[35]=text/richtext; class=java.io.InputStream; charset=UTF-16BE
+f[36]=text/richtext; class=java.nio.ByteBuffer; charset=UTF-16BE
+f[37]=text/richtext; class="[B"; charset=UTF-16BE
+f[38]=text/richtext; class=java.io.InputStream; charset=UTF-16LE
+f[39]=text/richtext; class=java.nio.ByteBuffer; charset=UTF-16LE
+f[40]=text/richtext; class="[B"; charset=UTF-16LE
+f[41]=text/richtext; class=java.io.InputStream; charset=ISO-8859-1
+f[42]=text/richtext; class=java.nio.ByteBuffer; charset=ISO-8859-1
+f[43]=text/richtext; class="[B"; charset=ISO-8859-1
+f[44]=text/richtext; class=java.io.InputStream; charset=US-ASCII
+f[45]=text/richtext; class=java.nio.ByteBuffer; charset=US-ASCII
+f[46]=text/richtext; class="[B"; charset=US-ASCII
+f[47]=application/x-java-serialized-object; class=java.lang.String
+f[48]=text/plain; class=java.io.Reader; charset=Unicode
+f[49]=text/plain; class=java.lang.String; charset=Unicode
+f[50]=text/plain; class=java.nio.CharBuffer; charset=Unicode
+f[51]=text/plain; class="[C"; charset=Unicode
+f[52]=text/plain; class=java.io.InputStream; charset=unicode
+f[53]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16
+f[54]=text/plain; class="[B"; charset=UTF-16
+f[55]=text/plain; class=java.io.InputStream; charset=UTF-8
+f[56]=text/plain; class=java.nio.ByteBuffer; charset=UTF-8
+f[57]=text/plain; class="[B"; charset=UTF-8
+f[58]=text/plain; class=java.io.InputStream; charset=UTF-16BE
+f[59]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16BE
+f[60]=text/plain; class="[B"; charset=UTF-16BE
+f[61]=text/plain; class=java.io.InputStream; charset=UTF-16LE
+f[62]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16LE
+f[63]=text/plain; class="[B"; charset=UTF-16LE
+f[64]=text/plain; class=java.io.InputStream; charset=ISO-8859-1
+f[65]=text/plain; class=java.nio.ByteBuffer; charset=ISO-8859-1
+f[66]=text/plain; class="[B"; charset=ISO-8859-1
+f[67]=text/plain; class=java.io.InputStream; charset=US-ASCII
+f[68]=text/plain; class=java.nio.ByteBuffer; charset=US-ASCII
+f[69]=text/plain; class="[B"; charset=US-ASCII
+
+
+Ubuntu Linux 8.04, jdk1.6.0, from Gnome file manager (one dir, one file or several files/dirs gives same result). æøå OK
+ f[0]=application/x-java-serialized-object; class=java.lang.String
+ f[1]=x-special/gnome-copied-files; class=java.io.InputStream
+ f[2]=text/plain; class=java.io.Reader; charset=Unicode
+ f[3]=text/plain; class=java.lang.String; charset=Unicode
+ f[4]=text/plain; class=java.nio.CharBuffer; charset=Unicode
+ f[5]=text/plain; class="[C"; charset=Unicode
+ f[6]=text/plain; class=java.io.InputStream; charset=unicode
+ f[7]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16
+ f[8]=text/plain; class="[B"; charset=UTF-16
+ f[9]=text/plain; class=java.io.InputStream; charset=UTF-8
+ f[10]=text/plain; class=java.nio.ByteBuffer; charset=UTF-8
+ f[11]=text/plain; class="[B"; charset=UTF-8
+ f[12]=text/plain; class=java.io.InputStream; charset=UTF-16BE
+ f[13]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16BE
+ f[14]=text/plain; class="[B"; charset=UTF-16BE
+ f[15]=text/plain; class=java.io.InputStream; charset=UTF-16LE
+ f[16]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16LE
+ f[17]=text/plain; class="[B"; charset=UTF-16LE
+ f[18]=text/plain; class=java.io.InputStream; charset=ISO-8859-1
+ f[19]=text/plain; class=java.nio.ByteBuffer; charset=ISO-8859-1
+ f[20]=text/plain; class="[B"; charset=ISO-8859-1
+ f[21]=text/plain; class=java.io.InputStream; charset=US-ASCII
+ f[22]=text/plain; class=java.nio.ByteBuffer; charset=US-ASCII
+ f[23]=text/plain; class="[B"; charset=US-ASCII
+
+
+Ubuntu Linux 8.04, jdk1.6.0, from Mozilla 3.0. THE HTML OPTIONS SEEMS BROKEN!
+ f[0]=text/html; class=java.io.Reader; charset=Unicode
+ f[1]=text/html; class=java.lang.String; charset=Unicode
+ f[2]=text/html; class=java.nio.CharBuffer; charset=Unicode
+ f[3]=text/html; class="[C"; charset=Unicode
+ f[4]=text/html; class=java.io.InputStream; charset=UTF-16
+ f[5]=text/html; class=java.nio.ByteBuffer; charset=UTF-16
+ f[6]=text/html; class="[B"; charset=UTF-16
+ f[7]=text/html; class=java.io.InputStream; charset=UTF-8
+ f[8]=text/html; class=java.nio.ByteBuffer; charset=UTF-8
+ f[9]=text/html; class="[B"; charset=UTF-8
+ f[10]=text/html; class=java.io.InputStream; charset=UTF-16BE
+ f[11]=text/html; class=java.nio.ByteBuffer; charset=UTF-16BE
+ f[12]=text/html; class="[B"; charset=UTF-16BE
+ f[13]=text/html; class=java.io.InputStream; charset=UTF-16LE
+ f[14]=text/html; class=java.nio.ByteBuffer; charset=UTF-16LE
+ f[15]=text/html; class="[B"; charset=UTF-16LE
+ f[16]=text/html; class=java.io.InputStream; charset=ISO-8859-1
+ f[17]=text/html; class=java.nio.ByteBuffer; charset=ISO-8859-1
+ f[18]=text/html; class="[B"; charset=ISO-8859-1
+ f[19]=text/html; class=java.io.InputStream; charset=US-ASCII
+ f[20]=text/html; class=java.nio.ByteBuffer; charset=US-ASCII
+ f[21]=text/html; class="[B"; charset=US-ASCII
+ f[22]=application/x-java-serialized-object; class=java.lang.String
+ f[23]=text/plain; class=java.io.Reader; charset=Unicode
+ f[24]=text/plain; class=java.lang.String; charset=Unicode
+ f[25]=text/plain; class=java.nio.CharBuffer; charset=Unicode
+ f[26]=text/plain; class="[C"; charset=Unicode
+ f[27]=text/plain; class=java.io.InputStream; charset=unicode
+ f[28]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16
+ f[29]=text/plain; class="[B"; charset=UTF-16
+ f[30]=text/plain; class=java.io.InputStream; charset=UTF-8
+ f[31]=text/plain; class=java.nio.ByteBuffer; charset=UTF-8
+ f[32]=text/plain; class="[B"; charset=UTF-8
+ f[33]=text/plain; class=java.io.InputStream; charset=UTF-16BE
+ f[34]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16BE
+ f[35]=text/plain; class="[B"; charset=UTF-16BE
+ f[36]=text/plain; class=java.io.InputStream; charset=UTF-16LE
+ f[37]=text/plain; class=java.nio.ByteBuffer; charset=UTF-16LE
+ f[38]=text/plain; class="[B"; charset=UTF-16LE
+ f[39]=text/plain; class=java.io.InputStream; charset=ISO-8859-1
+ f[40]=text/plain; class=java.nio.ByteBuffer; charset=ISO-8859-1
+ f[41]=text/plain; class="[B"; charset=ISO-8859-1
+ f[42]=text/plain; class=java.io.InputStream; charset=US-ASCII
+ f[43]=text/plain; class=java.nio.ByteBuffer; charset=US-ASCII
+ f[44]=text/plain; class="[B"; charset=US-ASCII
+ f[45]=text/x-moz-url-priv; class=java.io.InputStream
+ f[46]=text/_moz_htmlinfo; class=java.io.InputStream
+ f[47]=text/_moz_htmlcontext; class=java.io.InputStream
+ f[48]=text/x-moz-url-priv; class=java.nio.ByteBuffer
+ f[49]=text/_moz_htmlinfo; class=java.nio.ByteBuffer
+ f[50]=text/_moz_htmlcontext; class=java.nio.ByteBuffer
+ f[51]=text/x-moz-url-priv; class="[B"
+ f[52]=text/_moz_htmlinfo; class="[B"
+f[53]=text/_moz_htmlcontext; class="[B"
+
+
+
+
+ */
